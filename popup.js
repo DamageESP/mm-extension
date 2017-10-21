ba7c3548180db8b8896c95a77c7d8ed1b3406079
Víctor Campos <DamageESP@users.noreply.github.com>
2017-10-22 00:02:37 +0200

    mini ui

--- popup.js
+++ popup.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Get settings
   browser.storage.sync.get(["mm_devMode"], settings => {
     let devMode = document.querySelector('#devMode')
-    devMode.checked = settings.mm_devMode ? true : null
+    devMode.checked = settings.mm_devMode
     devMode.addEventListener('change', e => {
       settings.mm_devMode = devMode.checked
       browser.storage.sync.set(settings)
